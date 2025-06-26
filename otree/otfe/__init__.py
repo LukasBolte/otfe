d45db1112b0daeac27786156d0d50ba8c2c24ae8
Lukas Bolte <lukas.bolte@outlook.com>
2025-06-26 21:04:27 +0200

    Update __init__.py

--- otree/otfe/__init__.py
+++ otree/otfe/__init__.py
@@ -44,7 +44,9 @@ def creating_session(subsession: Subsession):
     if subsession.round_number == 1:
         treatments = ['C-Info','C-NoInfo', 'T1-T-Info', 'T1-T-NoInfo','T1-P','T2-T'] 
         random.shuffle(treatments)
+        print(treatments)
         treatments = itertools.cycle(treatments)
+        
         i=1
         for p in subsession.get_players():
             el = next(treatments)
@@ -528,7 +530,7 @@ class TaxInfo1(Page):
 
         if player.participant.treatment == 'C-Info':
             tax_info = "<p>Here is some information that you may want to take into account when thinking about future tax rates:</p> <p style='text-align: center; max-width: 90%; margin: auto;'>Other individuals were randomly selected for a <b>one-time</b> tax shock on last period’s earnings that <b>changed their tax rate to 50%</b>. As initially established, your <b>default tax rate for future work periods is still 25%</b>.</p>"
-        elif player.participant.treatment == 'T1-1-Info':
+        elif player.participant.treatment == 'T1-T-Info':
             tax_info = "<p>Here is some information that you may want to take into account when thinking about future tax rates:</p> <p style='text-align: center; max-width: 90%; margin: auto;'>Other individuals were also randomly selected for a <b>one-time</b> tax shock on last period’s earnings that <b>changed their tax rate to 50%</b>. As initially established, your <b>default tax rate for future work periods is still 25%</b>.</p>"
 
         return { 
@@ -537,7 +539,7 @@ class TaxInfo1(Page):
 
     @staticmethod
     def is_displayed(player: Player):
-        return player.participant.treatment in ['C-Info','T1-1-Info']
+        return player.participant.treatment in ['C-Info','T1-T-Info']
 
 
 class Beliefs2(Page):
